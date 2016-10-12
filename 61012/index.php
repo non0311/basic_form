e836d90ef94cd030785f96298f3ed9af2d26894e
non0311 <silencesuzuka0311@gmail.com>
2016-10-12 14:01:30 +0900

    confirm画面実装

--- 61012/index.php
+++ 61012/index.php
@@ -3,6 +3,30 @@
 //大枠の処理 POSTに値がなければ、input.htmlへいく。
 if (!empty($_POST)){
 
+    $form = $_POST;
+    $error = array();
+
+    if (empty($form['company'])) {
+        $error['company']="必須";
+    }
+
+    if (empty($form['tel'])) {
+        $error['tel']="必須";
+    }
+
+    if (empty($form['mail'])) {
+        $error['mail']="必須";
+    }
+
+
+    // エラーがあったら
+    // もう一度inputへいきexitする
+    if (!empty($error)) {
+        include_once("input.html");
+        exit;
+    }
+    //エラーがなければ確認画面へ遷移
+    include_once("confirm.html");
     exit;
 
 } //if (!empty($_POST))
