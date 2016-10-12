dd90899672bd8beee4cc79f4cbc314c3410a31bf
non0311 <silencesuzuka0311@gmail.com>
2016-10-12 17:41:05 +0900

    決定/戻るボタン実装完成、画面遷移の大枠完成

--- 61012_mission/index.php
+++ 61012_mission/index.php
@@ -20,13 +20,20 @@ if (empty($form['date'])) {
     $error['date']="必須";
 }
 
-// エラーがあったらもう一度inputへ
-if (!empty($error) ) {
+// エラーがある　or  戻るボタン押してたら  もう一度inputへ
+if (!empty($error)  || !empty($form['back']) ) {
     include_once("input.html");
     exit;
-}else{
-    //エラーなければ確認画面へ
+}
+
+//入力画面からエラーがない or 確認画面で送信ボタンを押してなかったら
+//確認画面へ遷移
+if (empty($form['send'])) {
     include_once("confirm.html");
     exit;
 }
+
+//確認画面から送信ボタンを押したら完了画面へ
+include_once("complete.html");
+exit;
 
